d60b0f5ea44a824aba2af9e22924551a236f9bcf
Victor Alex <victoralexsander5200@gmail.com>
2021-12-04 16:40:57 -0300

    commit inicial projeto java

---
+++ src/main/java/com/cerimonial/cerimonial/CerimonialApplication.java
@@ -0,0 +1,13 @@
+package com.cerimonial.cerimonial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerimonialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerimonialApplication.class, args);
+	}
+
+}
